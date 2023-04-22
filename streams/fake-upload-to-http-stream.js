49ef7257b2cdb16694039d87791663c862afae16
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-22 18:50:02 -0300

    :sparkles: feat: consuming a complete stream

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -22,4 +22,10 @@ fetch("http://localhost:3334", {
   method: "POST",
   body: new OneToHundredStream(),
   duplex: "half",
-});
+})
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    console.log(data);
+  });
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -11,8 +11,17 @@ class InverNumberStream extends Transform {
   }
 }
 
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverNumberStream()).pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent);
 });
 
 server.listen(3334);

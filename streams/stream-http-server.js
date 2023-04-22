ed74e16b90dea6430f5a32fdf353c51158db044c
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-22 18:41:39 -0300

    :sparkles: feat: aplying streams in http module

---
+++ streams/stream-http-server.js
@@ -0,0 +1,18 @@
+import http from "node:http";
+import { Transform } from "node:stream";
+
+class InverNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+
+    console.log(transformed);
+
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+const server = http.createServer((req, res) => {
+  return req.pipe(new InverNumberStream()).pipe(res);
+});
+
+server.listen(3334);

1dc10d7cfb83d2a4c32bc60c2588289475c903d5
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-22 13:59:56 -0300

    :sparkles: feat: saving users in memory

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { url, method } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("Listagem de usuários");
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+    });
     return res.end("Criação de usuários");
   }
 });

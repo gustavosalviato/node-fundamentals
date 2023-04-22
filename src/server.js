c4e275477b14a7f02badec6e6003ff64e484197c
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-22 14:09:21 -0300

    :wrench: chore: add http status code to routes

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const server = http.createServer((req, res) => {
 
   if (method === "GET" && url === "/users") {
     return res
-    .setHeader('Content-type', 'application/json')
-    .end(JSON.stringify(users));
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
@@ -17,8 +17,11 @@ const server = http.createServer((req, res) => {
       name: "John Doe",
       email: "johndoe@gmail.com",
     });
-    return res.end("Criação de usuários");
+    return res.writeHead(201).end();
   }
+
+
+  return res.writeHead(404).end()
 });
 
 server.listen(3333);

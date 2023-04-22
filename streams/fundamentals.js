b91b94167aef39044cfd60ec860e5f92b57ba45c
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-22 18:10:20 -0300

    :sparkles: feat: creating a readable  string

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -3,22 +3,19 @@ import { Readable } from "node:stream";
 class OneToHundredStream extends Readable {
   index = 1;
 
-  _read(){
-    const i = this.index++
+  _read() {
+    const i = this.index++;
 
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
 
-  setTimeout(() => {
-    if (i > 100){
-      this.push(null)
-    }else{
-      const buf = Buffer.from(String(i))
-
-      this.push(buf)
-    }
-  }, 1000);
+        this.push(buf);
+      }
+    }, 1000);
   }
 }
 
-
-new OneToHundredStream()
-  .pipe(process.stdout)
+new OneToHundredStream().pipe(process.stdout);

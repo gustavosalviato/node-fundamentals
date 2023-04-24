704001fff7b1fee08d477ed750b0f5031fc0a8ae
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-24 10:00:48 -0300

    :wrench: chore: adjust routes structure

--- src/server.js
+++ src/server.js
@@ -1,32 +1,18 @@
 import http from "node:http";
 import { json } from "../middlewares/json.js";
-import { Database } from "./database.js";
-import { randomUUID } from "node:crypto";
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { url, method } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
-    return res.end(JSON.stringify(users));
-  }
+  const route = routes.find((route) => {
+    return route.method === method && route.path === url;
+  });
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.writeHead(201).end();
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end();

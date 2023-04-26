07fbfa1a9d6adb1686ca03ae18e4843a23449089
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-26 15:08:14 -0300

    feat: add parameters regex

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Database } from "./database.js";
 import { randomUUID } from "node:crypto";
-
+import { buildRoutePath } from "./utils/build-route-path.js";
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const users = database.select("users");
       return res.end(JSON.stringify(users));
@@ -14,7 +14,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -27,6 +27,14 @@ export const routes = [
       database.insert("users", user);
 
       return res.writeHead(201).end();
+    },
+  },
+
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/):id"),
+    handler: (req, res) => {
+      return res.end();
     },
   },
 ];

9d790d9e90fe82ff30856d3e7511e265a3abfdde
gustavosalviato <gustavosalviato8@gmail.com>
2023-04-26 20:16:27 -0300

    feat: filtering list

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,13 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = database.select("users");
+      const { search } = req.query;
+      
+      const users = database.select("users", {
+        name: search,
+        email: search,
+      });
+
       return res.end(JSON.stringify(users));
     },
   },
